dc1bceddb9001f6fa8876061dc8bd4657059829a
Ashutosh <pashutosh563@email.com>
2025-04-07 16:45:59 +0530

    done

--- resources/views/user/profile/bindMail.blade.php
+++ resources/views/user/profile/bindMail.blade.php
@@ -157,7 +157,6 @@
                             </div>
                         </div>
 
-                        
                         <div data-v-1daeed5a="" data-v-6e2d35de="" class="go">
                             <div data-v-d8bb9fe2="" data-v-1daeed5a="" data-v-6e2d35de="" style="height: 1.4rem;">
                                 <div data-v-d8bb9fe2="" class="big_btn_box">
