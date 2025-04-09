db7e6e1d19ec26b09506bf47764ba17997baaa54
Rajneesh9034 <rajneshk2017@gmail.com>
2025-04-09 12:43:52 +0530

    done

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/clear1', function() {
+Route::get('/clear', function() {
 
    Artisan::call('cache:clear');
    Artisan::call('config:clear');
